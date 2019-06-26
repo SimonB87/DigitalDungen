1f0037c739551b7a3226ff7e238da90203b1ebb2
SimonB87 <buryans@gmail.com>
2019-06-26 23:57:17 +0200

    Improved movement preparations and added game results

--- basicui.js
+++ basicui.js
@@ -1,20 +1,34 @@
 $(document).ready(function(){
+    //set tile width
     var tileWidth = $("div.tile").css( "width" );
     $("div.tile").css("height", tileWidth);
-
     $( window ).resize(function() {
         $("div.tile").css("height", tileWidth);
     });
+    //prepare the map
+    hideMonsters();
 
-    blink_hero_icon();
     function blink_hero_icon() {
         $('i.hero').fadeOut(500);
         $('i.hero').fadeIn(500);
     };
 
+    function blink_monster_icon() {
+        $('.monster').fadeOut(250);
+        $('.monster').fadeIn(250);
+        $('.monster').fadeOut(250);
+        $('.monster').fadeIn(250);
+    };
+
     $('button#start').click(function() {
       $('i.hero').remove();
       $(hero.position + " .tile__centre").prepend( hero.heroAvatar );
+      blink_hero_icon();
+      blockUnusedDoors();
+      openUsedDoors();
+      hideMonsters();
+      showMonsters();
+      setTimeout( blink_monster_icon(), 100);
     });
 
     $('i.doors--north').click(function() {
@@ -45,6 +59,10 @@ $(document).ready(function(){
       toggleHeroInsideClass();
       blockUnusedDoors();
       openUsedDoors();
+      hideMonsters();
+      showMonsters();
+      checkForVictory();
+      blink_hero_icon();
     }
 
     function toggleHeroInsideClass() {
@@ -53,10 +71,40 @@ $(document).ready(function(){
     }
 
     function blockUnusedDoors(){
-      //$("div.tile--inside:not(:has(div.tile__centre.heroInside))").css("pointer-events", "none");
+      $('i.pathDoors').addClass("doorBlocked");
+    }
+
+    function hideMonsters(){
+      $('i.monster').addClass("hidden");
     }
 
     function openUsedDoors(){
-      //$("tile--inside  div.tile--inside:has(div.tile__centre.heroInside)").css("pointer-events", "auto");
+      var selector = 'div' + hero.position + '> div > i.pathDoors';
+      $( selector ).removeClass("doorBlocked");
+    }
+
+    function showMonsters(){
+      var selector = 'div' + hero.position + '> div.tile__centre > i.monster';
+      $( selector ).removeClass("hidden");
+    }
+
+    function checkForVictory(){
+      if ( hero.position == ".x5.y5" ) {
+
+        function postResult(){
+          $('#gameResultStatement').fadeOut(250);
+          $('#gameResultStatement').fadeIn(250);
+        }
+
+        function printResult(){
+          document.getElementById("gameResultStatement").innerHTML = 'You are the winner!!!';
+          postResult();
+          postResult();
+          postResult();
+          postResult();
+        }
+
+        setTimeout(printResult(), 300);
+      }
     }
 });
