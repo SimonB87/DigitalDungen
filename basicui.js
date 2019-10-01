e1221c613f7059b7b914b9bbddd91add32ffe2e7
SimonB87 <buryans@gmail.com>
2019-10-01 22:35:25 +0200

    edited code style, added prevention of modal disappearing

--- basicui.js
+++ basicui.js
@@ -1,140 +1,163 @@
-$(document).ready(function(){
-
+$(document).ready(function() {
   correctionOfBoardSize();
 
-    $(window).resize(function() {
-      correctionOfBoardSize();
-    });
+  $(window).resize(function() {
+    correctionOfBoardSize();
+  });
 
-    //prepare the map
+  //prepare the map
+  hideMonsters();
+
+  $("button#start").click(function() {
+    hero.position.x = 1;
+    hero.position.y = 1;
+    var heroPosition = ".x" + hero.position.x + ".y" + hero.position.y;
+
+    $("body")
+      .find("i.hero")
+      .remove();
+
+    $(heroPosition + " .tile__centre").prepend(hero.heroAvatar);
+    blink_hero_icon();
+    blockUnusedDoors();
+    openUsedDoors();
     hideMonsters();
+    showMonsters();
+    setTimeout(blink_monster_icon(), 100);
+  });
 
-    $('button#start').click(function() {
-      hero.position.x = 1;
-      hero.position.y = 1;
-      var heroPosition = ".x" + hero.position.x + ".y" + hero.position.y;
+  $("i.doors--north").click(function() {
+    $("body")
+      .find("i.hero")
+      .remove();
+    moveHero.moveHeroNorth();
+    moveHeroPreparation();
+  });
 
-      $("body").find("i.hero").remove();
+  $("i.doors--south").click(function() {
+    $("body")
+      .find("i.hero")
+      .remove();
+    moveHero.moveHeroSouth();
+    moveHeroPreparation();
+  });
 
-      $(heroPosition + " .tile__centre").prepend(hero.heroAvatar);
-      blink_hero_icon();
-      blockUnusedDoors();
-      openUsedDoors();
-      hideMonsters();
-      showMonsters();
-      setTimeout( blink_monster_icon(), 100);
-    });
+  $("i.doors--east").click(function() {
+    $("body")
+      .find("i.hero")
+      .remove();
+    moveHero.moveHeroEast();
+    moveHeroPreparation();
+  });
 
-    $('i.doors--north').click(function() {
-      $("body").find("i.hero").remove();
-      moveHero.moveHeroNorth();
-      moveHeroPreparation();
-    });
+  $("i.doors--west").click(function() {
+    $("body")
+      .find("i.hero")
+      .remove();
+    moveHero.moveHeroWest();
+    moveHeroPreparation();
+  });
 
-    $('i.doors--south').click(function() {
-      $("body").find("i.hero").remove();
-      moveHero.moveHeroSouth();
-      moveHeroPreparation();
-    });
+  function blink_monster_icon() {
+    $(".monster").fadeOut(250);
+    $(".monster").fadeIn(250);
+    $(".monster").fadeOut(250);
+    $(".monster").fadeIn(250);
+  }
 
-    $('i.doors--east').click(function() {
-      $("body").find("i.hero").remove();
-      moveHero.moveHeroEast();
-      moveHeroPreparation();
-    });
+  function moveHeroPreparation() {
+    toggleHeroInsideClass();
+    blockUnusedDoors();
+    openUsedDoors();
+    hideMonsters();
+    showMonsters();
+    checkForVictory();
+    blink_hero_icon();
+  }
 
-    $('i.doors--west').click(function() {
-      $("body").find("i.hero").remove();
-      moveHero.moveHeroWest();
-      moveHeroPreparation();
-    });
-
-    function blink_monster_icon() {
-        $('.monster').fadeOut(250);
-        $('.monster').fadeIn(250);
-        $('.monster').fadeOut(250);
-        $('.monster').fadeIn(250);
-    }
-
-    function moveHeroPreparation(){
-      toggleHeroInsideClass();
-      blockUnusedDoors();
-      openUsedDoors();
-      hideMonsters();
-      showMonsters();
-      checkForVictory();
-      blink_hero_icon();
-    }
-
-    function blink_hero_icon() {
-      $("body").find("i.hero").fadeOut(500);
-      $("body").find("i.hero").fadeIn(500);
+  function blink_hero_icon() {
+    $("body")
+      .find("i.hero")
+      .fadeOut(500);
+    $("body")
+      .find("i.hero")
+      .fadeIn(500);
   }
 });
 
-function correctionOfBoardSize(){
+function correctionOfBoardSize() {
   var windowHeight = window.innerHeight,
-      windowWidth = window.innerWidth,
-      headerHeight = document.querySelector("header").offsetHeight,
-      boardHeight = document.querySelector("#board").offsetHeight,
-      boardWidth = document.querySelector("#board").offsetWidth,
-      windowHeightDiff = windowHeight - headerHeight - boardHeight,
-      windowWidthDiff = windowWidth - boardWidth;
+    windowWidth = window.innerWidth,
+    headerHeight = document.querySelector("header").offsetHeight,
+    boardHeight = document.querySelector("#board").offsetHeight,
+    boardWidth = document.querySelector("#board").offsetWidth,
+    windowHeightDiff = windowHeight - headerHeight - boardHeight,
+    windowWidthDiff = windowWidth - boardWidth;
 
   if (windowHeightDiff > 0) {
     var boardWidth = $("#board").css("width");
     $("#board").css("height", boardWidth);
   } else {
     var maximumBoardSideLength = windowHeight - headerHeight;
-    $("#board").css("height", maximumBoardSideLength).css("width", maximumBoardSideLength);
+    $("#board")
+      .css("height", maximumBoardSideLength)
+      .css("width", maximumBoardSideLength);
   }
 
   if (windowWidthDiff < 0) {
     var maxWidth = $("#mainContainer").css("width");
-    $("#board").css("height", maxWidth).css("width", maxWidth);
+    $("#board")
+      .css("height", maxWidth)
+      .css("width", maxWidth);
   } else {
     var maximumBoardSideLength = windowHeight - headerHeight;
-    $("#board").css("height", maximumBoardSideLength).css("width", maximumBoardSideLength);
+    $("#board")
+      .css("height", maximumBoardSideLength)
+      .css("width", maximumBoardSideLength);
   }
-
 }
 
 function toggleHeroInsideClass() {
-  $('div.tile__centre').removeClass("heroInside");
-  $( "div.tile__centre:has(i.hero)" ).addClass( "heroInside" );
+  $("div.tile__centre").removeClass("heroInside");
+  $("div.tile__centre:has(i.hero)").addClass("heroInside");
 }
 
-function blockUnusedDoors(){
-  $('i.pathDoors').addClass("doorBlocked");
+function blockUnusedDoors() {
+  $("i.pathDoors").addClass("doorBlocked");
 }
 
-function hideMonsters(){
-  $('i.monster').addClass("hidden");
+function hideMonsters() {
+  $("i.monster").addClass("hidden");
 }
 
-function openUsedDoors(){
+function openUsedDoors() {
   var heroPosition = ".x" + hero.position.x + ".y" + hero.position.y,
-      selector = 'div' + heroPosition + '> div > i.pathDoors';
-  $( selector ).removeClass("doorBlocked");
+    selector = "div" + heroPosition + "> div > i.pathDoors";
+  $(selector).removeClass("doorBlocked");
 }
 
-function showMonsters(){
+function showMonsters() {
   var heroPosition = ".x" + hero.position.x + ".y" + hero.position.y,
-      selector = 'div' + heroPosition + '> div.tile__centre > i.monster';
-  $( selector ).removeClass("hidden");
+    selector = "div" + heroPosition + "> div.tile__centre > i.monster";
+  $(selector).removeClass("hidden");
 }
 
-function checkForVictory(){
+function checkForVictory() {
   var heroPosition = ".x" + hero.position.x + ".y" + hero.position.y;
-  if ( heroPosition == ".x5.y5" ) {
-
-
-    function printResult(){
-      document.getElementById("myModal--notice").innerHTML = 'You are the winner!!!';
+  if (heroPosition == ".x5.y5") {
+    function printResult() {
+      document.getElementById("myModal--notice").innerHTML =
+        "You are the winner!!!";
       document.getElementById("modalButton").click();
     }
 
-    $('i.pathDoors').addClass("doorBlocked");
+    //code for preventing modal from disappearing
+    $("#infoModal").modal({
+      backdrop: "static",
+      keyboard: false
+    });
+
+    $("i.pathDoors").addClass("doorBlocked");
 
     setTimeout(printResult(), 300);
   }
