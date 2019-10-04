e4a79f76487583f472a88e8f88c91ee5223d734f
SimonB87 <buryans@gmail.com>
2019-10-04 22:35:21 +0200

    Edited Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -13,3 +13,28 @@ gulp.task("less", function() {
     )
     .pipe(gulp.dest("./public/css"));
 });
+
+/* gulp.task("run", ["less"]);
+
+gulp.task("watch", function() {
+  gulp.watch("./style/*.less", ["less"]);
+});
+
+gulp.task("default", ["run", "watch"]); */
+
+gulp.task("default", run, watch);
+
+function run() {
+  return gulp
+    .src("./style/*.less")
+    .pipe(
+      less({
+        paths: [path.join(__dirname, "less", "includes")]
+      })
+    )
+    .pipe(gulp.dest("./public/css"));
+}
+
+function watch() {
+  gulp.watch("./style/*.less", ["less"]);
+}
