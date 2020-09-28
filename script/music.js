e4f48733877064d8d98555b65a28c9a7912dc93f
SimonB87 <buryans@gmail.com>
2020-09-28 15:59:42 +0200

    added audio music player

--- script/music.js
+++ script/music.js
@@ -1,2 +1,60 @@
-const myAudioElem = document.getElementById("myAudio");
-myAudioElem.volume = 0.25;
+musicObject = {
+  musicElem: document.getElementById("myAudio"),
+  volume: 0.1,
+  autoplay: function(){
+    this.musicElem.volume = this.volume;
+  },
+  play: function(){
+    this.musicElem.play();
+  },
+  pause: function(){
+    this.musicElem.pause();
+  },
+  volumeChange: function(num){
+    num = parseFloat(num);
+    this.volume = this.volume + num;
+    this.musicElem.volume = this.volume;
+  },
+  mute: function(){
+    this.volume = 0;
+    this.musicElem.volume = this.volume;
+  },
+  trackNumberPlaying: 0,
+  trackList: [
+    {mp3: "sawsquarenoise_-_01_-_Valiesse.mp3", ogg: "sawsquarenoise_-_01_-_Valiesse.ogg"},
+    {mp3: "sawsquarenoise_-_02_-_Fantasa_de_los_Pinos.mp3", ogg: "sawsquarenoise_-_02_-_Fantasa_de_los_Pinos.ogg"},
+    {mp3: "sawsquarenoise_-_03_-_El_Paraso_de_los_Troncos.mp3", ogg: "sawsquarenoise_-_03_-_El_Paraso_de_los_Troncos.ogg"},
+    {mp3: "sawsquarenoise_-_04_-_Fantasa_del_Bello_Alcazar.mp3", ogg: "sawsquarenoise_-_04_-_Fantasa_del_Bello_Alcazar.ogg"},
+    {mp3: "sawsquarenoise_-_05_-_Rito_Oculto.mp3", ogg: "sawsquarenoise_-_05_-_Rito_Oculto.ogg"}
+  ],
+  playOtherTrack: function(changeToTrack){
+
+    this.trackNumberPlaying = this.trackNumberPlaying + changeToTrack;
+    const numberOfTracksInList = this.trackList.length - 1;
+
+    if (this.trackNumberPlaying > numberOfTracksInList) {
+      this.trackNumberPlaying = 0;
+    }
+    if (this.trackNumberPlaying < 0) {
+      this.trackNumberPlaying = numberOfTracksInList;
+    }
+
+    const mp3Source = document.getElementById("sourceMp3");
+    const oggSource = document.getElementById("sourceOgg");
+
+    const currentUrl = window.location.href;
+
+    const mp3Src = currentUrl + "/music/" + this.trackList[this.trackNumberPlaying].mp3;
+    const oggSrc = currentUrl + "/music/" + this.trackList[this.trackNumberPlaying].ogg;
+
+    this.musicElem.pause();
+
+    mp3Source.src = mp3Src;
+    oggSource.src = oggSrc;
+
+    this.musicElem.play();
+
+  }
+}
+
+musicObject.autoplay();
