3b4ddb348278ef075be4c12f015b9092777b705a
SimonB87 <buryans@gmail.com>
2020-09-30 08:38:14 +0200

    added battle dialog

--- script/notices.js
+++ script/notices.js
@@ -10,7 +10,31 @@ const notices = {
     document.getElementById("modalButton").click();
   },
   showBattleDialog: function(){
-    //TODO show health bars
+    const selectorModalSection = "modalSectionThree";
+    const spaceInModal = document.getElementById(selectorModalSection);
+    const spaceModalContent = `<div class="battleDialog">
+          <div class="battleDialog--section battleDialog--hero">
+            <h4 class="textAlignCenter">Hero</h4>
+            <div id="heroHealthBar" class="figureBar healthBar">
+              <div id="heroHealthBarLevel" class="figureBarLevel healthBarLevel"></div>
+            </div>
+            <div id="heroManaBar" class="figureBar manaBar">
+              <div id="heroManaBarLevel" class="figureBarLevel manaBarLevel"></div>
+            </div>
+          </div>
+
+          <div class="battleDialog--section battleDialog--monster">
+            <h4 class="textAlignCenter">${round.currentMonster.type}</h4>
+            <div id="monsterHealthBar" class="figureBar healthBar">
+              <div id="monsterHealthBarLevel" class="figureBarLevel healthBarLevel"></div>
+            </div>
+            <div id="monsterManaBar" class="figureBar manaBar">
+              <div id="monsterManaBarLevel" class="figureBarLevel manaBarLevel"></div>
+            </div>
+          </div>
+
+        </div>`;
+    spaceInModal.innerHTML = spaceModalContent;
 
     //TODO show monster and hero stats
   }
