b5008b661a0c04f4b97bc69216138fe8faa9fa5d
SimonB87 <buryans@gmail.com>
2020-10-06 17:10:25 +0200

    added hero specs group, inventory, weapon, armor, spells

--- script/hero.js
+++ script/hero.js
@@ -7,10 +7,129 @@ hero = {
     x: 1,
     y: 1
   },
-  strength: 6,
-  defense: 4,
-  hitPoints: 100,
-  mana: 5,
+  specs: {
+    strength: 6,
+    defense: 4,
+    hitPoints: 100,
+    mana: 5,
+    countOfSpelsToChant: 1,
+    level: 1,
+    skillPoints: 0
+  },
+  inventory: [
+    {
+    name: "bread",
+    type: "consumable", 
+    kind: "food", 
+    usage: "heal", 
+    defence: 0, 
+    damage: 0, 
+    itemHitPoints: 1, 
+    itemWeight: 0.5,
+    hitPointsIncrease: 5, 
+    mana: 0, 
+    skillPoints: 0,
+    handsToUse: 0
+    }
+  ],
+  weapon: {
+    name: "short sword",
+    type: "item",
+    kind: "wearable",
+    usage: "weapon",
+    defence: 0, 
+    damage: 2,
+    itemHitPoints: 100,
+    itemWeight: 2,
+    hitPointsIncrease: 0, 
+    mana: 0, 
+    skillPoints: 0,
+    handsToUse: 1
+  },
+  armor: {
+    headGear: {
+      name: "leather hat",
+      type: "item",
+      kind: "wearable",
+      usage: "armor",
+      defense: 0.5,
+      damage: 0,
+      itemHitPoints: 100,
+      itemWeight: 0.5,
+      hitPointsIncrease: 0, 
+      mana: 0, 
+      skillPoints: 0,
+      handsToUse: 0
+    },
+    shield: {
+      name: "wooden shield",
+      type: "item",
+      kind: "wearable",
+      usage: "armor",
+      defense: 1,
+      damage: 0,
+      itemHitPoints: 100,
+      itemWeight: 2,
+      hitPointsIncrease: 0, 
+      mana: 0, 
+      skillPoints: 0,
+      handsToUse: 0
+    },
+    chestPlate: {
+      name: "tunica",
+      type: "item",
+      kind: "wearable",
+      usage: "armor",
+      defense: 1,
+      damage: 0,
+      itemHitPoints: 100,
+      itemWeight: 1,
+      hitPointsIncrease: 0, 
+      mana: 0, 
+      skillPoints: 0,
+      handsToUse: 0
+    },
+    shoes: {
+      name: "leather shoes",
+      type: "item",
+      kind: "wearable",
+      usage: "armor",
+      defense: 1,
+      damage: 0,
+      itemHitPoints: 100,
+      itemWeight: 0.5,
+      hitPointsIncrease: 0, 
+      mana: 0, 
+      skillPoints: 0,
+      handsToUse: 0
+    }
+  },
+  spells: {
+    chanted: {
+      name: "",
+      spellKind: "",
+      effect: {
+        increaseHealth: 0,
+        increaseStrength: 0,
+        increaseHitPoints: 0,
+        increaseDefence: 0,
+        manaConsuption: 0
+      }
+    },
+    known: [
+      {
+        name: "battle cry",
+        spellKind: "chant",
+        effect: {
+          increaseHealth: 0,
+          increaseStrength: 1,
+          increaseHitPoints: 0,
+          increaseDefence: 1,
+          manaConsuption: 1
+        }
+      }
+    ]
+  },
   situation: "",
   message: "",
   heroAvatar: "<i class='hero fas'>&#xf521;</i>",
